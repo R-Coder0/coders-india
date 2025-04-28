093951fdf9f608cf854ed95b8a1ff7f4b62ddde8
okaytrip134 <info@okaytrip.in>
2025-04-28 18:45:39 +0530

    changes

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -3,7 +3,7 @@ export default function TutorialsPage() {
     return (
       <div className="p-8">
         <h1 className="text-3xl font-bold mb-4">Tutorials</h1>
-        <p>Here you'll find programming tutorials categorized by topics.</p>
+        <p>Here you&apos;ll find programming tutorials categorized by topics.</p>
       </div>
     )
   }
